4a271cd93016edaeee56aa14f37a04d9dccb0e84
Mikkel J Ridley <mikkel250@gmail.com>
2017-11-17 08:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return 'I can\'t hear you!';
   }
-  if (string.toUpperCase === string) {
+  if (string.toUpperCase() === string) {
     return 'YES INDEED!'
   }
   
